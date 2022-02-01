a20a76cdb3f2db525f31f1a63300ef035d0a40c1
user160244980349 <mkuznetsov7991@gmail.com>
2022-02-01 23:47:38 +0000

    Some

--- configs/mariadb/init.sql
+++ configs/mariadb/init.sql
@@ -4,72 +4,75 @@ GRANT ALL PRIVILEGES ON iot_annotation.* TO iot_annotation;
 USE iot_annotation;
 
 CREATE TABLE `users` (
-    `id`        INT PRIMARY KEY AUTO_INCREMENT,
-    `name`      VARCHAR(255) UNIQUE,
-    `email`     VARCHAR(255) UNIQUE NOT NULL
+    `id`    INT PRIMARY KEY AUTO_INCREMENT,
+    `name`  VARCHAR(255) UNIQUE NOT NULL,
+    `email` VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE `passwords` (
-    `id`     INT PRIMARY KEY DEFAULT 0,
-    `value`  VARCHAR(255) NOT NULL,
-    FOREIGN KEY (id) REFERENCES `users`(id)
+    `id`    INT PRIMARY KEY DEFAULT 0,
+    `value` VARCHAR(255) NOT NULL,
+    FOREIGN KEY (`id`) REFERENCES `users`(`id`)
 );
 
 CREATE TABLE `groups` (
-    `id`        INT PRIMARY KEY AUTO_INCREMENT,
-    `name`      VARCHAR(255)
+    `id`   INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE `permissions` (
-    `id`        INT PRIMARY KEY AUTO_INCREMENT,
-    `for`       VARCHAR(255)
+    `id`  INT PRIMARY KEY AUTO_INCREMENT,
+    `for` VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE `group_user` (
-    `id`        INT PRIMARY KEY AUTO_INCREMENT,
-    `group_id`  INT,
-    `user_id`   INT,
-    FOREIGN KEY (group_id)    REFERENCES `groups`(id),
-    FOREIGN KEY (user_id)     REFERENCES `users`(id)
+    `id`       INT PRIMARY KEY AUTO_INCREMENT,
+    `group_id` INT NOT NULL,
+    `user_id`  INT NOT NULL,
+    FOREIGN KEY (`group_id`) REFERENCES `groups`(`id`),
+    FOREIGN KEY (`user_id`)  REFERENCES `users`(`id`),
+    UNIQUE (`group_id`, `user_id`)
 );
 
 CREATE TABLE `group_permission` (
     `id`            INT PRIMARY KEY AUTO_INCREMENT,
-    `group_id`      INT,
-    `permission_id` INT,
-    FOREIGN KEY (group_id)        REFERENCES `groups`(id),
-    FOREIGN KEY (permission_id)   REFERENCES `permissions`(id)
+    `group_id`      INT NOT NULL,
+    `permission_id` INT NOT NULL,
+    FOREIGN KEY (`group_id`)      REFERENCES `groups`(`id`),
+    FOREIGN KEY (`permission_id`) REFERENCES `permissions`(`id`),
+    UNIQUE (`group_id`, `permission_id`)
 );
 
-CREATE TABLE `texts` (
-    `id`            INT PRIMARY KEY AUTO_INCREMENT,
-    `url`           MEDIUMTEXT,
-    `manufacturer`  VARCHAR(256),
-    `keyword`       VARCHAR(256),
-    `website`       MEDIUMTEXT,
-    `policy`        VARCHAR(256),
-    `policy_hash`   VARCHAR(256),
-    `content`       MEDIUMTEXT
+CREATE TABLE `policies` (
+    `id`        INT PRIMARY KEY AUTO_INCREMENT,
+    `hash`      VARCHAR(255) UNIQUE NOT NULL,
+    `requested` INT DEFAULT 0,
+    `annotated` INT DEFAULT 0,
+    `content`   MEDIUMTEXT
 );
 
--- CREATE TABLE `selections` (
---     `id`                    INT PRIMARY KEY AUTO_INCREMENT,
---     `starts_on`             INT,
---     `ends_on`               INT,
---     `text_id`               INT,
---     `parent_id`             INT DEFAULT 0,
---     `selection_class`       VARCHAR(255) NOT NULL,
---     FOREIGN KEY (text_id)             REFERENCES `texts`(id),
---     FOREIGN KEY (parent_id)           REFERENCES `selections`(id)
--- );
+CREATE TABLE `products` (
+    `id`           INT PRIMARY KEY AUTO_INCREMENT,
+    `manufacturer` VARCHAR(255),
+    `keyword`      VARCHAR(255),
+    `product_url`  MEDIUMTEXT,
+    `website_url`  MEDIUMTEXT,
+    `policy_url`   VARCHAR(255),
+    `policy_hash`  VARCHAR(255),
+    FOREIGN KEY (`policy_hash`) REFERENCES `policies`(`hash`)
+);
 
--- CREATE TABLE `user_selection` (
---     `id` INT PRIMARY KEY AUTO_INCREMENT,
---     `user_id`                       INT,
---     `selection_id`                  INT,
---     FOREIGN KEY (user_id)         REFERENCES `users`(id),
---     FOREIGN KEY (selection_id)    REFERENCES `selections`(id)
--- );
+CREATE TABLE `selections` (
+    `id`              INT PRIMARY KEY AUTO_INCREMENT,
+    `starts_on`       INT,
+    `ends_on`         INT,
+    `selection_class` VARCHAR(255) NOT NULL,
+    `user_id`         INT,
+    `policy_hash`     VARCHAR(255),
+    `created_at`      DATETIME DEFAULT NOW(),
+    FOREIGN KEY (`user_id`)     REFERENCES `users`(`id`),
+    FOREIGN KEY (`policy_hash`) REFERENCES `policies`(`hash`)
+);
 
 -- Seeds --
 INSERT INTO `users` (
@@ -98,7 +101,7 @@ INSERT INTO `permissions` (
 ('manage-groups'),
 ('manage-data');
 
-INSERT INTO group_permission (
+INSERT INTO `group_permission` (
     `group_id`,
     `permission_id`
 ) VALUES
--- readme.md
+++ readme.md
@@ -2,4 +2,3 @@ This is PHP app for iot-dataset annotation
 ==================
 
 # TODO
-- [ ] Nothing yet
