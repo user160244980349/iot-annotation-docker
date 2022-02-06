a0c9f840ef09925320305712e03bd4516712879c
user160244980349 <mkuznetsov7991@gmail.com>
2022-02-06 12:09:29 +0000

    Copy env.php

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 git clone https://github.com/user160244980349/iot-annotation.git php-fpm/iot-annotation
+cp php-fpm/iot-annotation/example.env.php php-fpm/iot-annotation/env.php
 
 sudo docker-compose build
