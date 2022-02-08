9d6e8ad67ab24e098c514d5a40fb91c095e05ae3
user160244980349 <mkuznetsov7991@gmail.com>
2022-02-08 11:46:51 +0000

    Npm container

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+git clone https://github.com/user160244980349/iot-annotation-frontend.git nginx/iot-annotation-frontend
 git clone https://github.com/user160244980349/iot-annotation.git php-fpm/iot-annotation
+
 cp php-fpm/iot-annotation/example.env.php php-fpm/iot-annotation/env.php
 
 sudo docker-compose build
